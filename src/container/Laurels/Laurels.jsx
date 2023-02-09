900347c02c5c1ab57f94eff1fc1ce0d894234e21
bastosnz74 <80914441+bastosnz74@users.noreply.github.com>
2023-02-09 17:47:49 +0100

    Update Laurels.jsx

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -17,6 +17,7 @@ const AwardCard = ({ award: { imgUrl, title, subtitle } }) => (
 const Laurels = () => (
   <div className="app__bg app__wrapper section__padding" id="awards">
     <div className="app__wrapper_info">
+      <SubHeading title="Récompenses" />
       <h1 className="headtext__cormorant">Nos récompenses</h1>
 
       <div className="app__laurels_awards">
