3fb31eb243cea06b8289ea2dc84cd85f522e5827
bastosnz74 <80914441+bastosnz74@users.noreply.github.com>
2023-02-09 17:44:34 +0100

    Update Newsletter.jsx

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -6,6 +6,7 @@ import './Newsletter.css';
 const Newsletter = () => (
   <div className="app__newsletter">
     <div className="app__newsletter-heading">
+      <SubHeading title="Newsletter" />
       <h1 className="headtext__cormorant">Newsletter</h1>
       <p className="p__opensans">Ne ratez rien des nouveaux menus !</p>
     </div>
